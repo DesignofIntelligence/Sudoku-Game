29a0b41052fb957a3f580cf32988798b961ceb1a
OmarAyman85 <49420230+OmarAyman85@users.noreply.github.com>
2019-04-19 06:02:33 +0200

    buttons

--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -2,6 +2,7 @@ package sudoku;
 
 import javafx.geometry.Insets;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -14,6 +15,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -46,14 +48,54 @@ class game extends BorderPane {
         settings.setTextFill(Color.RED);
         settings.setPrefSize(200, 20);
         settings.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        settings handler2 = new settings();
+        settings.setOnAction(handler2);
         Button exit = new Button("EXIT");
         exit.setTextFill(Color.RED);
         exit.setPrefSize(200, 20);
         exit.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        exit handler3 = new exit();
+        exit.setOnAction(handler3);
         options1.getChildren().add(play);
         options1.getChildren().add(settings);
         options1.getChildren().add(exit);
 //..............................................................
+    }
+}
+//-----------------------------------------------------------------
+class play implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("play button clicked");
+        game1 gm1 = new game1();
+        Scene sc1 = new Scene(gm1, 500, 500);
+        Stage s = new Stage();
+        s.setScene(sc1);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
+        
+            }
+}
+//-----------------------------------------------------------------
+
+class settings implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("settings button clicked");
+     
+    }
+}
+//-----------------------------------------------------------------
+
+class exit implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("exit button clicked");
     }
 }
 //-----------------------------------------------------------------
@@ -79,10 +121,14 @@ class game1 extends BorderPane {
         standard.setTextFill(Color.RED);
         standard.setPrefSize(200, 20);
         standard.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        standard handler1 = new standard();
+        standard.setOnAction(handler1);
         Button timetrial = new Button("TIME TRIAL");
         timetrial.setTextFill(Color.RED);
         timetrial.setPrefSize(200, 20);
         timetrial.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        timetrial handler2 = new timetrial();
+        timetrial.setOnAction(handler2);
         Label lbl = new Label("Please Choose a Game Mode");
         lbl.setStyle("-fx-background-color:null;");
         lbl.setTextFill(Color.BLUE);
@@ -93,13 +139,71 @@ class game1 extends BorderPane {
 //..............................................................
         VBox back1 = new VBox();
         this.setBottom(back1);
-        back1.setPadding(new Insets(0, 150, 100, 150));
+        back1.setPadding(new Insets(0, 0, 100, 0));
+        back1.setAlignment(Pos.TOP_CENTER);
         Button back = new Button("BACK");
         back.setTextFill(Color.RED);
         back.setPrefSize(200, 20);
         back.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        back1 handler3 = new back1();
+        back.setOnAction(handler3);
         back1.getChildren().add(back);
 //..............................................................        
+    }
+}
+//-----------------------------------------------------------------
+
+
+class standard implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("standard button clicked");
+        game2 gm2 = new game2();
+        Scene sc2 = new Scene(gm2, 500, 500);
+        Stage s = new Stage();
+        s.setScene(sc2);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
+        
+    }
+}
+//-----------------------------------------------------------------
+
+class timetrial implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("time trial button clicked");
+        game2 gm2 = new game2();
+        Scene sc2 = new Scene(gm2, 500, 500);
+        Stage s = new Stage();
+        s.setScene(sc2);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
+        
+    }
+}
+//-----------------------------------------------------------------
+
+class back1 implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("back1 button clicked");
+        game gm = new game();
+        Scene sc = new Scene(gm, 500, 500);
+        Stage s = new Stage();
+        s.setScene(sc);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
+        
     }
 }
 //-----------------------------------------------------------------
@@ -125,14 +229,20 @@ class game2 extends BorderPane {
         easy.setTextFill(Color.RED);
         easy.setPrefSize(200, 20);
         easy.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        easy handler1 = new easy();
+        easy.setOnAction(handler1);
         Button medium = new Button("MEDIUM");
         medium.setTextFill(Color.RED);
         medium.setPrefSize(200, 20);
         medium.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        medium handler2 = new medium();
+        medium.setOnAction(handler2);
         Button hard = new Button("HARD");
         hard.setTextFill(Color.RED);
         hard.setPrefSize(200, 20);
         hard.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        hard handler3 = new hard();
+        hard.setOnAction(handler3);
         Label lbl = new Label("Please select the difficulty");
         lbl.setStyle("-fx-background-color:null;");
         lbl.setTextFill(Color.BLUE);
@@ -144,77 +254,33 @@ class game2 extends BorderPane {
 //..............................................................
         VBox back1 = new VBox();
         this.setBottom(back1);
-        back1.setPadding(new Insets(0, 150, 100, 150));
-        //back1.setAlignment(Pos.CENTER);
+        back1.setPadding(new Insets(0, 0, 100, 0));
+        back1.setAlignment(Pos.TOP_CENTER);
         Button back = new Button("BACK");
         back.setTextFill(Color.RED);
         back.setPrefSize(200, 20);
         back.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+        back2 handler4 = new back2();
+        back.setOnAction(handler4);
         back1.getChildren().add(back);
 //..............................................................
     }
 }
-//-----------------------------------------------------------------
-
-class play implements EventHandler<ActionEvent> {
-
-    @Override
-    public void handle(ActionEvent e) {
-        System.out.println("start button clicked");
-    }
-}
-//-----------------------------------------------------------------
-
-class settings implements EventHandler<ActionEvent> {
-
-    @Override
-    public void handle(ActionEvent e) {
-        System.out.println("settings button clicked");
-    }
-}
-//-----------------------------------------------------------------
-
-class exit implements EventHandler<ActionEvent> {
-
-    @Override
-    public void handle(ActionEvent e) {
-        System.out.println("exit button clicked");
-    }
-}
-//-----------------------------------------------------------------
-
-class standard implements EventHandler<ActionEvent> {
-
-    @Override
-    public void handle(ActionEvent e) {
-        System.out.println("standard button clicked");
-    }
-}
-//-----------------------------------------------------------------
-
-class timetrial implements EventHandler<ActionEvent> {
-
-    @Override
-    public void handle(ActionEvent e) {
-        System.out.println("time trial button clicked");
-    }
-}
-//-----------------------------------------------------------------
-
-class back1 implements EventHandler<ActionEvent> {
-
-    @Override
-    public void handle(ActionEvent e) {
-        System.out.println("back1 button clicked");
-    }
-}
-//-----------------------------------------------------------------
+//----------------------------------------------------------------
 
 class easy implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent e) {
         System.out.println("easy button clicked");
+        game3 gm3 = new game3();
+        Scene Sc3 = new Scene(gm3,500,500);
+        Stage s = new Stage();
+        s.setScene(Sc3);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
     }
 }
 //-----------------------------------------------------------------
@@ -224,6 +290,14 @@ class medium implements EventHandler<ActionEvent> {
     @Override
     public void handle(ActionEvent e) {
         System.out.println("medium button clicked");
+        game3 gm3 = new game3();
+        Scene Sc3 = new Scene(gm3,500,500);
+        Stage s = new Stage();
+        s.setScene(Sc3);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
     }
 }
 //-----------------------------------------------------------------
@@ -233,6 +307,15 @@ class hard implements EventHandler<ActionEvent> {
     @Override
     public void handle(ActionEvent e) {
         System.out.println("hard button clicked");
+        game3 gm3 = new game3();
+        Scene Sc3 = new Scene(gm3,500,500);
+        Stage s = new Stage();
+        s.setScene(Sc3);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
+        
     }
 }
 //-----------------------------------------------------------------
@@ -242,9 +325,90 @@ class back2 implements EventHandler<ActionEvent> {
     @Override
     public void handle(ActionEvent e) {
         System.out.println("back2 button clicked");
+        game1 gm1 = new game1();
+        Scene Sc1 = new Scene(gm1,500,500);
+        Stage s = new Stage();
+        s.setScene(Sc1);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
+        
     }
 }
 //-----------------------------------------------------------------
+
+class game3 extends BorderPane{
+    public game3(){
+VBox options4 = new VBox();
+    this.setCenter(options4);
+    options4.setSpacing(10);
+    options4.setAlignment(Pos.CENTER);
+    
+//..............................................................
+    HBox hints = new HBox();
+    this.setBottom(hints);
+    hints.setPadding(new Insets(25,50,100,0));
+    hints.setAlignment(Pos.TOP_RIGHT);
+    hints.setSpacing(10);
+    Button reset = new Button("RESET");
+    reset.setTextFill(Color.BLUE);
+    reset.setPrefSize(50,20);
+    reset.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+    reset handler2 = new reset();
+    reset.setOnAction(handler2);
+    Button hint = new Button("HINT");
+    hint.setTextFill(Color.BLUE);
+    hint.setPrefSize(50,20);
+    hint.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+    hint handler1 = new hint();
+    hint.setOnAction(handler1);
+    Button back = new Button("Back");
+    back.setTextFill(Color.BLUE);
+    back.setPrefSize(50,20);
+    back.setStyle("-fx-border-color:orange;" + "-fx-background-color:null;" + "-fx-border-radius: 10;");
+    back3 handler3 = new back3();
+    back.setOnAction(handler3);
+    hints.getChildren().add(reset);
+    hints.getChildren().add(hint);
+    hints.getChildren().add(back);
+    }
+}
+
+//-----------------------------------------------------------------
+
+class hint implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("hint button clicked");
+    }
+}
+//-----------------------------------------------------------------
+class reset implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("reset button clicked");
+    }
+}
+//-----------------------------------------------------------------
+class back3 implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("back1 button clicked");
+        game gm = new game();
+        Scene sc = new Scene(gm, 500, 500);
+        Stage s = new Stage();
+        s.setScene(sc);
+        s.setTitle("Sudoku");
+        s.sizeToScene();
+        s.show();
+        s.setResizable(true);
+        
+    }
+}
 
 public class Sudoku extends Application {
     
@@ -252,21 +416,19 @@ public class Sudoku extends Application {
     public void start(Stage primaryStage) {
 
         game gm = new game();
-        game1 gm1 = new game1();
-        game2 gm2 = new game2();
         Scene sc = new Scene(gm, 500, 500);
-        Scene sc1 = new Scene(gm1, 500, 500);
-        Scene sc2 = new Scene(gm2, 500, 500);
         primaryStage.setScene(sc);
         primaryStage.setTitle("Sudoku");
         primaryStage.sizeToScene();
         primaryStage.show();
         primaryStage.setResizable(true);
+        
     }
+    
+    
 //------------------------------------------------------------------    
 
     public static void main(String[] args) {
-        System.out.println("Application:-");
         launch(args);
 
     }
